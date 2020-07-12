94a70801c37c3c07017129884e615886e68f8976
Kevin Kraydich <kraydich@umich.edu>
2020-07-12 14:41:18 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='iot-farm',
+    version='1.0',
+    description='Small farming project that relies on feedback from sensors to control parameters for optimal plant growth.',
+    license='MIT',
+    author='Kevin Kraydich',
+    author_email='kevin.kraydich@gmail.com',
+    packages=['iot-farm'],
+    install_requires=[],
+    scripts=[]
+)
