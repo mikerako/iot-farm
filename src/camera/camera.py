325f3518cde15eec2e5308de0bdc42de3a684ee4
mrako <mrako@umich.edu>
2020-07-20 19:24:21 -0400

    Fixed timestamp issue. Added camera modules to requirements

--- src/camera/camera.py
+++ src/camera/camera.py
@@ -41,67 +41,17 @@ class Camera:
             if key == ord("q"):
                 break
         end_time = datetime.now()
-        time_passed = calculate_time_passed(start_time, end_time)
-        logging.debug("Video stream lasted", time_passed, "long")
+        time_passed_str,_ = calculate_time_passed(start_time, end_time)
+        logging.debug("Video stream lasted", time_passed_str, "long")
 
     def calculate_time_passed(datetime start, datetime end):
-        years_passed = end.year - start.year
-        months_passed = end.month - start.month
-        days_passed = end.day - start.day
-        hours_passed = end.hour - start.hour
-        minutes_passed = end.minute - start.minute
-        seconds_passed = end.second - start.second
+        time_passed = end - start_time
         
-        xtra_days = 0
-        leap_yr_count = 0
-        curr_month = start.month
-        curr_year = start.year
-        while(curr_month != end.month && curr_year <= end.year):
-            if(curr_month == 1):
-                xtra_days += 31
-            if(curr_month == 2):
-                if(curr_year % 4 == 0 && curr_year % 100 != 0):
-                    xtra_days += 29
-                    leap_yr_count += 1
-                else:
-                    xtra_days += 28
-            if(curr_month == 3):
-                xtra_days += 31
-            if(curr_month == 4):
-                xtra_days += 30
-            if(curr_month == 5):
-                xtra_days += 31
-            if(curr_month == 6):
-                xtra_days += 30
-            if(curr_month == 7):
-                xtra_days += 31
-            if(curr_month == 8):
-                xtra_days += 31
-            if(curr_month == 9):
-                xtra_days += 30
-            if(curr_month == 10):
-                xtra_days += 31
-            if(curr_month == 11):
-                xtra_days += 30
-            if(curr_month == 12):
-                xtra_days += 31
-                curr_year += 1
-            
-            curr_month += 1
-             
-        total_seconds = seconds_passed + 60*minutes_passed + 3600*hours_passed + 
-                     86400*days_passed + xtra_days
-
-        time_gone = {"year(s)": years_passed}
-        total_seconds -= (years_passed * (365*86400) + 86400*leap_yr_count)
-        time_gone["month(s)"] = months_passed
-        total_seconds -= (xtra_days)
-        time_gone["day(s)"] = total_seconds % 86400
-        total_seconds -= time_gone["day(s)"] * 86400
-        time_gone["hour(s)"] = total_seconds % 3600
-        total_seconds -= time_gone["hour(s)"] * 3600
-        time_gone["minute(s)"] = minutes_passed
-        total_seconds -= time_gone["minute(s)"] * 60
-        time_gone["second(s)"] = total_seconds
+        time_str =  time_passed.year, "year(s),",
+                    time_passed.month, "month(s),", 
+                    time_passed.day, "day(s),", 
+                    time_passed.hour, "hour(s),", 
+                    time_passed.minute, "minute(s), and", 
+                    time_passed.second, "second(s)" 
         
-        return time_gone            
+        return time_str, time_passed          
