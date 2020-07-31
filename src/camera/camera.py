6527d88a84843fb515b8a5fc2ec87d5dc8396d2b
Michael Rakowiecki <mrako@umich.edu>
2020-07-31 00:42:52 -0400

    Tested and fixed cam image and stream issues

--- src/camera/camera.py
+++ src/camera/camera.py
@@ -7,6 +7,7 @@ from picamera import PiCamera
 import time
 import os
 from datetime import datetime
+import logging
 import cv2
 import cv2.aruco as aruco
 import numpy as np
@@ -22,36 +23,52 @@ class Camera:
         # warmup the camera
         time.sleep(2)
 
-    def cap_image():
-        image = camera.capture(self.rawCapture, format="bgr", use_video_port=True)
-        dtime = datetime.now()
-        logging.debug("Captured Image @ "+"%d-%d-%d-%d %d:%d:%d", dtime.year, 
-            dtime.month, dtime.day, dtime.hour, dtime.minute, dtime.second)
-        return image, dtime
+    def cap_image(self):
+        # image = self.camera.capture(self.rawCapture, format="bgr", use_video_port=True)
+        # dtime = datetime.now()
+        # logging.debug("Captured Image @ "+"%d-%d-%d-%d %d:%d:%d", dtime.year, 
+        #     dtime.month, dtime.day, dtime.hour, dtime.minute, dtime.second)
+        # return image, dtime
+        self.camera.start_preview()
+        self.camera.capture('/home/pi/Desktop/image_test.jpg')
+        self.camera.stop_preview()
 
-    def vid_stream():
+    def vid_stream(self):
+        self.camera.resolution = (640, 480)
+        self.rawCapture = PiRGBArray(self.camera, size=(640, 480))
         start_time = datetime.now()
-        for frame in camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
+        for frame in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
             image = np.array(frame.array)
             # show the frame
             cv2.imshow("Farm Location", image)
             key = cv2.waitKey(1) & 0xFF
             # clear the stream in preparation for the next frame
-            rawCapture.truncate(0)
+            self.rawCapture.truncate(0)
             # if the `q` key was pressed, break from the loop
             if key == ord("q"):
                 break
         end_time = datetime.now()
-        time_passed_str = format_time(end_time - start_time)
-        logging.debug("Video stream lasted ", time_passed_str)
+        time_passed_str = self.format_time(end_time - start_time)
+        logging.debug("Video stream lasted", time_passed_str)
+        print("Video stream lasted", time_passed_str)
+        self.rawCapture = PiRGBArray(self.camera, size=(1920, 1080))
 
-    def format_time(time: datetime):        
-        time_str =  time.year, "year(s),",
-                    time.month, "month(s),", 
-                    time.day, "day(s),", 
-                    time.hour, "hour(s),", 
-                    time.minute, "minute(s), and", 
-                    time.second, "second(s)"
+    def format_time(self, time: datetime):
+        tot_sec = time.total_seconds()
+        days = tot_sec // 86400
+        tot_sec -= 86400*days
+        hours = tot_sec // 3600
+        tot_sec -= 3600*hours
+        mins = tot_sec // 60
+        tot_sec -= 60*mins
+        secs = tot_sec
+
+        print("days", days)
+        print("hours", hours)
+        print("minuts", mins)
+        print("seconds", secs)
+
+        time_str = days, "day(s),", hours, "hour(s),", mins, "minute(s), and", secs, "second(s)"
         
         return time_str 
 
@@ -60,8 +77,9 @@ def main() -> None:
     img_folder = os.path.join(path, "images")
     cam = Camera()
     for idx in range(0,10):
-        im, time = cam.cap_image()
-        cv2.imwrite(img_folder + str(idx) + "__" + str(time) + ".png", im)
+        cam.cap_image()
+        # im, time = cam.cap_image()
+        # cv2.imwrite(img_folder + str(idx) + "__" + str(time) + ".png", im)
     
     cam.vid_stream()
 
