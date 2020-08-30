123577e32f3093b1a5c39d822c34114b2282c99e
Kevin Kraydich <kraydich@umich.edu>
2020-08-30 13:31:36 -0400

    added rudimentary flask UI

--- src/helpers/csv.py
+++ src/helpers/csv.py
@@ -1,21 +1,42 @@
-import datetime
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
+import os
+
+class Property:
+    def __init__(self, name, unit):
+        self.name = name
+        self.unit = unit
 
 class CSVProcessor:
     def __init__(self, filename: str):
         self._filename = filename
+        self._data = np.genfromtxt(filename, delimiter=',', dtype=None, names=True, encoding=None)
     
-    def read_data(self):
-        self._data = np.genfromtxt(self._filename, delimiter=',', names=True)
-        print(self._data)
+    def make_graphs(self):
+        graph_filenames = []
+        time = Property('time', 'HR:MIN:SEC')
+        properties = [
+            Property(name, unit) for name, unit in [
+                ('temperature', '°C'),
+                ('humidity', 'RH'),
+                ('pressure', 'Pa'),
+                ('co2', 'ppm')
+            ]
+        ]
 
-    def graph(self, data: np.array):
-        pass
+        for prop in properties:
+            graph_data(self._data['timestamp'], self._data[prop.name], time, prop)
+            filename = '{}.png'.format(prop.name)
+            graph_filenames.append(filename)
 
-def main():
-    csv = CSVProcessor('data/test.csv')
-    csv.read_data()
+        return graph_filenames            
 
-if __name__ == "__main__":
-    main()
+    
+
+def graph_data(xdata: np.array, ydata: np.array, xprop: Property, yprop: Property):
+    plt.plot(xdata, ydata)
+    plt.xlabel('{} ({})'.format(xprop.name, xprop.unit))
+    plt.ylabel('{} ({})'.format(yprop.name, yprop.unit))
+    plt.title('{} as a function of {}'.format(yprop.name, xprop.name))
+    plt.savefig('src/static/{}.png'.format(yprop.name))
+    plt.close()
