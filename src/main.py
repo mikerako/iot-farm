7e979d775d0d7fb92db13c3c8513370671ad0562
Kevin Kraydich <kraydich@umich.edu>
2020-11-01 13:49:36 -0500

    fixed text cooldown not working properly

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ Driver code for IoT farm backend.
 
 Authors:
 Kevin Kraydich <kevin.kraydich@gmail.com>
-Michael Rakowiecki <kevin.kraydich@gmail.com>
+Michael Rakowiecki
 '''
 
 from helpers import alerts, upload, user, csv, sensor, camera
@@ -21,8 +21,8 @@ DATA_PATH = os.path.join(os.getcwd(), '../data')
 LOG_PATH = os.path.join(os.getcwd(), '../log')
 
 # Open logging file inside log/
-logging.basicConfig(level=logging.INFO, filename=os.path.join(LOG_PATH, '{}.log'.format(time.strftime('%Y_%m_%d'))))
-last_notification_time = None
+# logging.basicConfig(level=logging.INFO, filename=os.path.join(LOG_PATH, '{}.log'.format(time.strftime('%Y_%m_%d'))))
+next_notification_time = datetime.datetime.now()
 
 # Open private config files
 with open(os.path.join(SOURCE_PATH, 'config-kevin.json')) as f:
@@ -30,17 +30,22 @@ with open(os.path.join(SOURCE_PATH, 'config-kevin.json')) as f:
 
 # Returns true if the current time is within the interval that
 # the lights are on, false otherwise.
-def are_lights_on() -> bool:
+def lights_on() -> bool:
     current = datetime.datetime.now().time()
     start = current.replace(7, 0, 0)
     end = current.replace(12 + 6, 0, 0)
     return start <= current <= end
 
+# Returns the threshold for temperature based on the state of the lights.
+def get_temperature_threshold():
+    return (70, 85) if lights_on() else (58, 70)
+
 # Checks to see when the last text notification was sent out. This
 # is to prevent sending too many texts at one time. Returns true if
 # we have not sent a text in a while, otherwise it returns false.
-def check_last_notification(time: time) -> bool:
-    return not last_notification_time or time <= last_notification_time + datetime.timedelta(hours=8)
+def check_next_notification(t: time) -> bool:
+    print(next_notification_time)
+    return t > next_notification_time
 
 # Checks a sensor reading for any values outside of the given min and
 # max thresholds. Returns a string containing an appropriate message if
@@ -53,6 +58,9 @@ def alert_threshold(reading: dict, thresholds: dict):
             msg += format_text(key, reading[key], True)
         elif reading[key] > max_threshold:
             msg += format_text(key, reading[key], False)
+    # Remove the final newline character
+    if msg:
+        msg = msg[:-1]
     return msg
 
 # Formats a message based on whether the reading was too high or too low.
@@ -81,30 +89,34 @@ def send_text_alert(recipients: list, reading: dict, thresholds: dict) -> None:
     now = datetime.datetime.now()
     msg = alert_threshold(reading, thresholds)
     # Check when the last notification was sent
-    if check_last_notification(datetime.datetime.now()) and msg:
+    if check_next_notification(now) and msg:
+        print('Updating next notification time...')
         # Update the time of last notification sent
-        last_notification_time = now
+        global next_notification_time
+        next_notification_time = now + datetime.timedelta(hours=8)
+        print(next_notification_time)
         # Send the message to all users
-        job_text(recipients, msg)
+        print('SENDING TEXT...')
+        # job_text(recipients, msg)
 
-def job_read(recipients: list, sensors: list, files: list, thresholds: dict) -> None:
+def job_read(recipients: list, sensors: list, files: list) -> None:
     average = {
-        'timestamp': 0,
         'temperature': 0,
         'humidity': 0,
         'pressure': 0
     }
 
+    thresholds = {
+        'temperature': get_temperature_threshold()
+    }
+
     # Read the current values from all sensors
     for i in range(len(sensors)):
-        print('reading sensor...')
         data = sensors[i].read_bme()
-        print(data)
-        print('done reading sensor')
 
         # Update the running average
-        for key in data.keys():
-            average[key] = (data[key] + i * average[key]) / (i + 1)
+        for key in average.keys():
+            average[key] = (float(data[key]) + i * average[key]) / (i + 1)
 
         # Write the results to the appropriate file
         line = '{},{},{},{}\n'.format(
@@ -141,7 +153,9 @@ def job_email(recipients: list) -> None:
 
 def job_picture(camera):
     img = camera.cap_image()
-    cv2.imwrite(DATA_PATH + time.strftime('%Y_%m_%d') + ".png", img)
+    filename = time.strftime('%Y_%m_%d') + ".png"
+    path = os.path.join(DATA_PATH, filename)
+    cv2.imwrite(path, img)
 
 def job_upload(files: list) -> None:
     up = upload.Uploader(CONFIG['upload'])
@@ -159,22 +173,24 @@ def main():
     sensors = [sensor.EnvComboSensor(0)]
     csv_files = [os.path.join(DATA_PATH, get_filename(i)) for i in range(len(sensors))]
     cam = camera.Camera()
-    thresholds = {
-        'temperature': (58, 85),
-        'humidity': (0, 100)
-    }
+    # thresholds = {
+    #     'temperature': get_temperature_threshold()
+    #     'humidity': (0, 100)
+    # }
 
     for f in csv_files:
         reset_file(f)
 
     schedule.every(10).seconds.do(
-        job_read, recipients=users.get_numbers(), sensors=sensors, files=csv_files, thresholds=thresholds
+        job_read, recipients=users.get_numbers(), sensors=sensors, files=csv_files
     )
-    schedule.every(30).seconds.do(job_picture, camera=cam)
+    schedule.every(10).seconds.do(job_picture, camera=cam)
+
+    # schedule.every(30).seconds.do(job_email, recipients=users.get_emails())
+    # schedule.every(30).seconds.do(job_upload, files=csv_files)
+
     # schedule.every().day.at("23:55").do(job_email, recipients=users.get_emails())
-    schedule.every(30).seconds.do(job_email, recipients=users.get_emails())
     # schedule.every().day.at("21:18").do(job_upload, files=csv_files)
-    schedule.every(30).seconds.do(job_upload, files=csv_files)
 
     while True:
         try:
